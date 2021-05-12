3c6f7b32d284b52d2f8c40e3430dac24a6db61c7
Daniel Melo <melo.daniel00@gmail.com>
2021-05-11 22:14:13 -0300

    CONTROLE_SÉRIES - Usando jobs. Concluído sistema de controle de séries.

--- app/Services/RemovedorDeSerie.php
+++ app/Services/RemovedorDeSerie.php
@@ -2,19 +2,31 @@
 namespace App\Services;
 
 use App\{Serie, Temporada, Episodio};
+use App\Events\SerieApagada;
+use App\Jobs\ExcluirCapaSerie;
 use Illuminate\Support\Facades\DB;
-
+use Storage;
 class RemovedorDeSerie
 {
     public function removerSerie(int $serieId): string
     {
         $nomeSerie = '';
-        DB::transaction(function () use ($serieId, &$nomeSerie) {
+        DB::transaction(function () use ($serieId, &$nomeSerie)
+        {
             $serie = Serie::find($serieId);
+            $serieObj = (object) $serie->toArray();
             $nomeSerie = $serie->nome;
 
             $this->removerTemporadas($serie);
             $serie->delete();
+
+            # Criando evento da série que foi apagada
+            $evento = new SerieApagada($serieObj);
+
+            # Emite o evento
+            event($evento);
+
+            ExcluirCapaSerie::dispatch($serieObj);
         });
 
         return $nomeSerie;
